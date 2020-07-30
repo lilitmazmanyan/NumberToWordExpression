40ec4b367b14c83ddf4fcddddbe2b79bc4758e5b
Lilit Mazmanyan <lilit.mazmanyan1@gmail.com>
2020-07-29 18:41:44 -0700

    Added driver code

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(convertNumberToWords.convertToWords(120));
+        System.out.println(convertNumberToWords.convertToWords(12_350));
+        System.out.println(convertNumberToWords.convertToWords(487));
+        System.out.println(convertNumberToWords.convertToWords(0));
+        System.out.println(convertNumberToWords.convertToWords(5_220_487));
+//        System.out.println(convertNumberToWords.convertToWords(-1));   // Throws an exception
+    }
+
+}
