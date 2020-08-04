3c2e3f349a1710da85b2dbde453fe51856c645c8
Lilit Mazmanyan <lilit.mazmanyan1@gmail.com>
2020-08-03 19:56:07 -0700

    Added more test cases

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
-public class Main {
+class Main {
     public static void main(String[] args) {
         System.out.println(convertNumberToWords.convertToWords(120));
         System.out.println(convertNumberToWords.convertToWords(12_350));
+        System.out.println(convertNumberToWords.convertToWords(12350));
         System.out.println(convertNumberToWords.convertToWords(487));
         System.out.println(convertNumberToWords.convertToWords(0));
         System.out.println(convertNumberToWords.convertToWords(5_220_487));
+        System.out.println(convertNumberToWords.convertToWords(5220487));
+        System.out.println(convertNumberToWords.convertToWords(20000001));
+        System.out.println(convertNumberToWords.convertToWords(1000001));
 //        System.out.println(convertNumberToWords.convertToWords(-1));   // Throws an exception
     }
-
 }
